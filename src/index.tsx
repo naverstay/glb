859e1fe26323507d77d9dc02215702b65de69023
nv <naverstay.me@gmail.com>
2022-12-04 20:08:05 +0100

    show how to on first visit

--- src/index.tsx
+++ src/index.tsx
@@ -4,21 +4,6 @@ import "./index.scss";
 import App from "./App";
 import {ThemeProvider} from "./context/ThemeContext";
 
-(function () {
-    let src = '//cdn.jsdelivr.net/npm/eruda';
-    if (!/eruda=1/.test(window.location.search) && localStorage.getItem('active-eruda') !== 'true') return;
-    console.log('eruda');
-    document.write('<script src="' + src + '"></script>');
-    document.write('<script>eruda.init();</script>');
-})();
-
-if (process.env.PUBLIC_URL) {
-    let script = document.createElement("script");
-    script.type = "text/javascript";
-    script.innerHTML = 'window.globeImageUrl="https://morsetranslator.org/images";';
-    document.getElementsByTagName('head')[0].appendChild(script);
-}
-
 ReactDOM.render(
     <React.StrictMode>
         <ThemeProvider>
