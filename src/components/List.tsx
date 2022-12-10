232f9a17915c5fc1fdd3f58509dfa0a9a0449a91
nv <naverstay.me@gmail.com>
2022-12-10 15:04:53 +0100

    country data clean up

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -120,7 +120,7 @@ export default function List({guesses, answerName, win, globeRef, practiceMode, 
                             <div className="suggestion-list__name"
                                  onClick={(e) => turnToCountry(e, idx)}>
                                 <div className="suggestion-list__flag">
-                                    <img src={`https://flagcdn.com/w40/${flag.toLowerCase()}.png`} alt={name}/>
+                                    <img src={`${process.env.PUBLIC_URL}/images/flags/${flag.toLowerCase()}.png`} alt={name}/>
                                 </div>
                                 <span>{name}</span>
                             </div>
